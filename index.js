be4e378ef0bb6f16f87dbeeecded986a2de97f9a
Lilian K <lilos4203@gmail.com>
2026-04-13 10:44:38 +0300

    fixed city name display from API response

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-let now= new Date
-let days=[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-
-if (minutes < 10) {
-    minutes = "0" + minutes;
-}
-
-let currentTime = `${day} ${hours}:${minutes}`;
-document.querySelector("#current-date").innerHTML = currentTime;
 let userForm= document.querySelector("#user-form")
 userForm.addEventListener("submit", (e)=>{
     e.preventDefault()
     let input= document.querySelector("#city-input")
-    let city= input.value
+    let cityInput= input.value
     let cityName= document.querySelector("#city-name")
-    cityName.innerHTML=city
+    cityName.innerHTML=cityInput
 
-    let sheApis = `https://api.shecodes.io/weather/v1/current?query=${city}&units=metric&key=65foc199tb00f3ddbce1a005364275f2`;
+    let sheApis = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&units=metric&key=65foc199tb00f3ddbce1a005364275f2`;
 
     axios.get(sheApis).then((res) => {
         let temp = Math.round(res.data.temperature.current);
-
+        cityName.innerHTML = res.data.city;
         let timeStamp= res.data.time*1000
         let date= new Date(timeStamp)
         let days = [
@@ -51,8 +31,15 @@ userForm.addEventListener("submit", (e)=>{
 
         let beTime= `${day} ${hours}:${minutes}`
         let currentDate= document.querySelector("#current-date")
-        currentDate.innerHTML=beTime|| currentTime
+        currentDate.innerHTML=beTime
+        document.querySelector("#description").innerHTML =
+            res.data.condition.description;
 
+        document.querySelector("#humidity").innerHTML =
+            `${res.data.temperature.humidity}%`;
+
+        document.querySelector("#wind").innerHTML =
+            `${res.data.wind.speed} m/s`;
 
         let iconCode = res.data.condition.icon;
 
@@ -75,3 +62,21 @@ userForm.addEventListener("submit", (e)=>{
 
 
 })
+let now = new Date();
+
+let days = [
+    "Sunday","Monday","Tuesday","Wednesday",
+    "Thursday","Friday","Saturday"
+];
+
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+
+let currentTime = `${day} ${hours}:${minutes}`;
+
+document.querySelector("#current-date").innerHTML = currentTime;
